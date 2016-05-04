8c57fb7b770016e6e88e1749194dd309f9b8403f
ktsn <ktsn55@gmail.com>
2016-05-04 14:01:22 +0900

    Define Action type

--- src/Data/Store.purs
+++ src/Data/Store.purs
@@ -6,9 +6,11 @@ import Control.Monad.State (execState, modify, State)
 
 type Subscriber s = s -> Eff () Unit
 
+type Action a = { type :: String | a }
+
 newtype Store s a = Store
   { state       :: s
-  , updater     :: s -> a -> s
+  , updater     :: s -> Action a -> s
   , subscribers :: Array (Subscriber s)
   }
 
@@ -17,17 +19,17 @@ type StoreState s a = State (Store s a) Unit
 subscribe :: forall s a. Subscriber s -> StoreState s a
 subscribe f = modify \(Store s) -> Store s { subscribers = s.subscribers ++ [f] } :: Store s a
 
-update :: forall s a. a -> StoreState s a
+update :: forall s a. Action a -> StoreState s a
 update a = modify \(Store s) -> Store s { state = s.updater s.state a } :: Store s a
 
-dispatch :: forall s a. Store s a -> a -> Eff () (Store s a)
+dispatch :: forall s a. Store s a -> Action a -> Eff () (Store s a)
 dispatch s a = do
   let next = execState (update a) s
   case next of
     Store n -> foreachE n.subscribers \sub -> sub n.state
   return next
 
-create :: forall s a. s -> (s -> a -> s) -> Store s a
+create :: forall s a. s -> (s -> Action a -> s) -> Store s a
 create s f = Store
   { state       : s
   , updater     : f
